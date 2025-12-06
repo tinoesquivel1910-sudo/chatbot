ec565f1328da2603c2d43a99ec05ef9bd43e9de6
tinoesquivel1910-sudo <tino.esquivel1910@gmail.com>
2025-12-06 11:33:02 -0300

    esta todo bien

--- analizador.py
+++ analizador.py
@@ -1,22 +1,16 @@
 def data(eleccion):
-    if eleccion == "1":
-        total = "las clases empiezan a las 10:00 y terminan a las 11:30"
-    elif eleccion == "2":
-       print("las clases cuestan 45.000 pesos al mes")
-    elif eleccion == "3":
-        print("el profe no tiene telefono(al menos no publico)")
+    if eleccion == 1:
+        print("el concierto empiza a las 22:30")
+    elif eleccion == 2:
+        print("las entradas cuestan 60.000")
+    elif eleccion == 3:
+        print("no hay entradas suficientes,va a tener q ir reventa")
+    elif eleccion == 4:
+        print("estan muy caras las enradas, ¿seguro que las quieres comprar?")
+    elif eleccion == 5:
+        print("¿las vas a comprar ahora? o te las reservamos?")        
     else:
-        print("no te entiendo")
+        print("cuanto cuestan las entradas de reventa?")
+    
 
-eleccion = int(input("""hola, soy un chatbot. estas son las preguntas que puedes hacerme:
-                    1: horarios de clase
-                     2:precio de clases
-                     3:telefono del profe
-                     4: salir""" ))
-while eleccion != 4:
-    data(eleccion)
-    print("""hola, soy un chatbot. estas son las preguntas que puedes hacerme:
-                    1: horarios de clase
-                     2:precio de clases
-                     3:telefono del profe
-                     4: salir""")
+
--- holamundo.py
+++ holamundo.py
@@ -1,14 +1,16 @@
-def data(eleccion):
-    if eleccion == "1":
-        total="el concirto empiza a las 22:30"
-    elif eleccion =="2":
-        print("las entradas cuestan 60.000")
-    elif eleccion =="3":
-        print("no hay enntradas suficientes,va a tener q ir reventa")
-    else:
-        print("cuanto cuestan las entradas de reventa?")
-eleccion = int(input(""" hola, soy tu chatbot de las entreadas del concierto                    
-                            1:Horario del concierto
-                            2:precio de las entradas
-                            3:cuato estan?
-                            4:salir"""))
+from analizador import data
+
+mensajeGenerico = """hola, soy tu chatbot de las entreadas del concierto                    
+1:Horario del concierto
+2:precio de las entradas
+3:cuato estan?
+4:si las quiero comprar
+5:si las voy a comprar ahora
+6: salir
+"""
+
+eleccion = int(input(mensajeGenerico))
+
+while eleccion != 6:
+    data(eleccion)
+    eleccion = int(input(mensajeGenerico))
